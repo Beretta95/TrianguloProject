8f89ffa110af334b3e12329e3384a1fb785c60b9
fabricio_beretta <fabricio_beretta@LTF0WTVG2.sicredi.net.br>
2020-04-27 16:46:34 -0300

    Primeiro Commit

---
+++ ProjetoTriangulo/src/main/java/pucrs/si/qp/ProjetoTriangulo/App.java
@@ -0,0 +1,13 @@
+package pucrs.si.qp.ProjetoTriangulo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
